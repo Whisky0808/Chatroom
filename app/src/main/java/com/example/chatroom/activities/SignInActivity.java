13f6b9abc6cbe2c6ce20f2d1801b38da547b79cf
Whisky0808 <kyra.yli@outlook.com>
2024-04-10 13:33:55 -0400

    initial commit

---
+++ app/src/main/java/com/example/chatroom/activities/SignInActivity.java
@@ -0,0 +1,33 @@
+package com.example.chatroom.activities;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.chatroom.R;
+import com.example.chatroom.databinding.ActivitySignInBinding;
+
+public class SignInActivity extends AppCompatActivity {
+
+    private ActivitySignInBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivitySignInBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        setListeners();
+
+    }
+
+    private void setListeners() {
+        binding.textCreateNewAccount.setOnClickListener(v->
+                startActivity(new Intent(getApplicationContext(), SignUpActivity.class)));
+    }
+}
---
+++ app/src/main/java/com/example/chatroom/activities/SignUpActivity.java
@@ -0,0 +1,30 @@
+package com.example.chatroom.activities;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.chatroom.R;
+import com.example.chatroom.databinding.ActivitySignUpBinding;
+
+public class SignUpActivity extends AppCompatActivity {
+
+    private ActivitySignUpBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivitySignUpBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        setListeners();
+    }
+
+    private void setListeners(){
+        binding.buttonSignIn.setOnClickListener(v->onBackPressed());
+    }
+}
