f78f74c69b254b2523a00e5abe79e5c0e16340c8
Whisky0808 <kyra.yli@outlook.com>
2024-04-14 23:27:31 -0400

    get users list

--- app/src/main/java/com/example/chatroom/activities/UsersActivity.java
+++ app/src/main/java/com/example/chatroom/activities/UsersActivity.java
@@ -1,26 +1,99 @@
 package com.example.chatroom.activities;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 import com.example.chatroom.R;
+import com.example.chatroom.adapters.UsersAdapter;
+import com.example.chatroom.databinding.ActivityUsersBinding;
+import com.example.chatroom.models.User;
+import com.example.chatroom.utilities.Constants;
+import com.example.chatroom.utilities.PreferenceManager;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class UsersActivity extends AppCompatActivity {
+
+    private ActivityUsersBinding binding;
+    private PreferenceManager preferenceManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_users);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        binding = ActivityUsersBinding.inflate(getLayoutInflater());
+        // if we dont't bind getRoot(), the element won't be selected and
+        setContentView(binding.getRoot());
+        preferenceManager = new PreferenceManager(getApplicationContext());
+        setListeners();
+        getUsers();
     }
+
+
+    private void setListeners() {
+
+        binding.imageBack.setOnClickListener(v ->
+            onBackPressed());
+    }
+
+
+// chatting list, get the chatList
+    private void getUsers(){
+
+        loading(true);
+        FirebaseFirestore database = FirebaseFirestore.getInstance();
+        database.collection(Constants.KEY_COLLECTION_USERS)
+                .get()
+                .addOnCompleteListener(task -> {
+                    loading(false);
+                    // get current userId
+                    String currentUserId = preferenceManager.getString(Constants.KEY_USER_ID);
+                    if (task.isSuccessful() && task.getResult() != null) {
+                        List<User> users = new ArrayList<>();
+                        for (QueryDocumentSnapshot queryDocumentSnapshot : task.getResult()) {
+                            if (currentUserId.equals(queryDocumentSnapshot.getId())) {
+                                continue; // avoid add current itself
+                            }
+                            User user = new User();
+                            user.name = queryDocumentSnapshot.getString(Constants.KEY_NAME);
+                            user.email = queryDocumentSnapshot.getString(Constants.KEY_EMAIL);
+                            user.image = queryDocumentSnapshot.getString(Constants.KEY_IMAGE);
+                            user.token = queryDocumentSnapshot.getString(Constants.KEY_FCM_TOKEN);
+                            user.id = queryDocumentSnapshot.getId();
+                            users.add(user);
+                        }
+                        if (!users.isEmpty()) {
+                            Log.d("msg","we have user");
+                            UsersAdapter usersAdapter = new UsersAdapter(users,this);
+                            binding.usersRecyclerView.setAdapter(usersAdapter);
+                            binding.usersRecyclerView.setVisibility(View.VISIBLE);
+                        } else {
+                            showErrorMessage();
+                        }
+                    } else {
+                        showErrorMessage();
+                    }
+                });
+
+    }
+
+    private void showErrorMessage() {
+        binding.textErrorMessage.setText(String.format("%s", "No user available"));
+        binding.textErrorMessage.setVisibility(View.VISIBLE);
+    }
+
+
+    private void loading(Boolean isLoading) {
+        if (isLoading) {
+            binding.progressBar.setVisibility(View.VISIBLE);
+        } else {
+            binding.progressBar.setVisibility(View.INVISIBLE);
+        }
+    }
+
 }
